18fca58be1dbaef4afe7462e11ea0d4a55b57cea
izhuravel <ivanguravel@gmail.com>
2023-02-21 00:22:14 +0200

    few changes

--- service.tf
+++ service.tf
@@ -21,8 +21,8 @@ resource "aws_ecs_task_definition" "my_first_task" {
             "password": "${var.db_password}"
           }
       ],
-      "memory": 2048,
-      "cpu": 2
+      "memory": 512,
+      "cpu": 256
     }
   ]
   DEFINITION
