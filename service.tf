4ef5803f51595a6dda705602030b17ba3e6aa527
izhuravel <ivanguravel@gmail.com>
2023-02-22 13:24:11 +0200

    few changes

--- service.tf
+++ service.tf
@@ -83,7 +83,8 @@ resource "aws_ecs_service" "my_first_service" {
   cluster         = "${aws_ecs_cluster.my_cluster.id}"             # Referencing our created Cluster
   task_definition = "${aws_ecs_task_definition.my_first_task.arn}" # Referencing the task our service will spin up
   launch_type     = "FARGATE"
-  desired_count   = 3 # Setting the number of containers to 3
+  desired_count   = 1 # Setting the number of containers to 3
+  health_check_grace_period_seconds = 30000000
 
   load_balancer {
     target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our target group
